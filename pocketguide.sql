c102f10883b4d94d782beaf8cab243a0745696e1
unknown <nmcgmata@donbosco.edu.ph>
2022-07-25 21:15:22 -0700

    Initial Commit

--- pocketguide.sql
+++ pocketguide.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 12, 2022 at 09:08 PM
+-- Generation Time: Jul 26, 2022 at 06:14 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -24,6 +24,27 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbl_announcement`
+--
+
+CREATE TABLE `tbl_announcement` (
+  `a_id` int(100) NOT NULL,
+  `date_posted` varchar(100) NOT NULL,
+  `a_author` varchar(200) NOT NULL,
+  `a_subject` varchar(200) NOT NULL,
+  `a_content` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_announcement`
+--
+
+INSERT INTO `tbl_announcement` (`a_id`, `date_posted`, `a_author`, `a_subject`, `a_content`) VALUES
+(1, '2022-07-26T03:46:13+00:00', '1', 'Typhoon', 'No class');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user_accounts`
 --
 
@@ -40,12 +61,17 @@ CREATE TABLE `user_accounts` (
 --
 
 INSERT INTO `user_accounts` (`uid`, `email`, `fullname`, `password`, `user_type`) VALUES
-(1, 'mata@gmail.com', 'nessa', '123', '1'),
-(2, 'mata@gmail.com', 'nessa', '123', '1');
+(1, 'mata@gmail.com', 'nessa', '123', '1');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `tbl_announcement`
+--
+ALTER TABLE `tbl_announcement`
+  ADD PRIMARY KEY (`a_id`);
 
 --
 -- Indexes for table `user_accounts`
@@ -61,7 +87,7 @@ ALTER TABLE `user_accounts`
 -- AUTO_INCREMENT for table `user_accounts`
 --
 ALTER TABLE `user_accounts`
-  MODIFY `uid` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `uid` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
